725b5048de1efbe61b0e151131268c5b40a3e4e3
edgar <edbenson@hotmail.com>
2013-05-28 21:36:24 -0700

    Added kernel options.
    Change-Id: I4813e6cb673ad5f744c5a488af57b35a59f88f02

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,14 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 #TARGET_KERNEL_CONFIG := j1a-perf_defconfig
 #TARGET_KERNEL_SOURCE := kernel/lge/gee
 
-TARGET_KERNEL_CONFIG := geebca_defconfig
-TARGET_KERNEL_SOURCE := kernel/lge/geem
+#TARGET_KERNEL_CONFIG := geebca_defconfig
+#TARGET_KERNEL_SOURCE := kernel/lge/geem
+
+#TARGET_KERNEL_CONFIG := geeb_defconfig
+#TARGET_KERNEL_SOURCE := kernel/lge/smoocha
+
+TARGET_KERNEL_CONFIG := geeb-fs_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/Houcha
 
 # inherit from gee-common
 -include device/lge/gee-common/BoardConfigCommon.mk
